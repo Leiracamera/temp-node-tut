00a99ac057ad346d8ee54d9061b418fe8b1be08e
Ariel Cameron <ariel.cameron@gmail.com>
2024-08-13 18:58:15 -0700

    Initial commit

---
+++ 2-globals.js
@@ -0,0 +1,12 @@
+// GLOBALS - NO WINDOW
+//(use console.log() to view i.e. console.log(__dirname))
+
+// __dirname - path to current directory
+// __filename - file name
+// require - function to use modules (CommonJS)
+// module - info about current module (file) 
+// process - info about env where the program is being executed
+
+console.log(__dirname)
+console.log(__filename)
+console.log(module)
---
+++ 4-names.js
@@ -0,0 +1,8 @@
+// local
+const secret = 'super secret'
+
+// share (with rest of app)
+const john = 'john'
+const peter = 'peter'
+
+module.exports = {john, peter}
